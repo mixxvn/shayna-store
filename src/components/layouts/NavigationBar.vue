30cf1f3ab957448a3e9272c3b050ed82ec065991
mixxvn <unitonework@gmail.com>
2022-12-14 11:12:55 +0700

    change static to dynamic element

--- src/components/layouts/NavigationBar.vue
+++ src/components/layouts/NavigationBar.vue
@@ -30,49 +30,40 @@
                                 Keranjang Belanja &nbsp;
                                 <a href="#">
                                     <i class="icon_bag_alt"></i>
-                                    <span>3</span>
+                                    <span>{{ cart.length }}</span>
                                 </a>
                                 <div class="cart-hover">
                                     <div class="select-items">
                                         <table>
-                                            <tbody>
-                                                <tr>
+                                            <tbody v-if="(cart.length > 0)">
+                                                <tr v-for="(item, index) in cart" :key="item.id">
                                                     <td class="si-pic">
-                                                        <img src="@/assets/img/select-product-1.jpg" alt="" />
+                                                        <img class="photo-cart" :src="item.photo" alt="" />
                                                     </td>
                                                     <td class="si-text">
                                                         <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
+                                                            <p>{{ item.price }} x 1</p>
+                                                            <h6>{{ item.name }}</h6>
                                                         </div>
                                                     </td>
-                                                    <td class="si-close">
+                                                    <td @click="removeItemInCart(index)" class="si-close">
                                                         <i class="ti-close"></i>
                                                     </td>
-                                                </tr>
+                                                </tr>                                                
+                                            </tbody>
+                                            <tbody v-else>
                                                 <tr>
-                                                    <td class="si-pic">
-                                                        <img src="@/assets/img/select-product-2.jpg" alt="" />
-                                                    </td>
-                                                    <td class="si-text">
-                                                        <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
-                                                        </div>
-                                                    </td>
-                                                    <td class="si-close">
-                                                        <i class="ti-close"></i>
-                                                    </td>
+                                                    <td>Keranjang kosong</td>
                                                 </tr>
                                             </tbody>
                                         </table>
                                     </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5>$120.00</h5>
+                                        <h5>{{ totalPrice }}</h5>
                                     </div>
                                     <div class="select-button">
-                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                                        <RouterLink to="/cart" class="primary-btn view-card">VIEW CARD</RouterLink>
                                         <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
                                     </div>
                                 </div>
@@ -92,6 +83,38 @@ import { RouterLink } from 'vue-router';
 export default{
     components: {
         RouterLink
+    },
+    data() {
+        return {
+            cart: []
+        }
+    },
+    methods: {
+        removeItemInCart(index){
+            this.cart.splice(index, 1)
+            const parsed = JSON.stringify(this.cart)
+            localStorage.setItem('user_cart', parsed)
+        }
+    },
+    mounted() {
+        if(localStorage.getItem('user_cart')){
+            try {
+                this.cart = JSON.parse(localStorage.getItem('user_cart'))
+            } catch (error) {
+                localStorage.removeItem('user_cart')
+            }
+        }
+    },
+    computed: {
+        totalPrice(){
+            return this.cart.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0 )
+        }
     }
 }
 </script>
+
+<style>
+.photo-cart{
+    width: 80px; 
+}
+</style>
