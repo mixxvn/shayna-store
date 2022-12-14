c4a3c2cc3511866e53aa6c3af6046d0bcb7ae65e
mixxvn <unitonework@gmail.com>
2022-12-14 11:19:56 +0700

    add: add to cart feature

--- src/components/homepage/ProductItem.vue
+++ src/components/homepage/ProductItem.vue
@@ -1,11 +1,10 @@
 <template>
-    <!-- <h1>{{product}}</h1> -->
     <div class="product-item">
         <div class="pi-pic">
             <img :src="product.galleries[0].photo" alt="" />
             <ul>
                 <li class="w-icon active">
-                    <a href="#"><i class="icon_bag_alt"></i></a>
+                    <a class="saveInCart" @click="saveInCart(product.id, product.name, product.galleries[0].photo, product.price)"><i class="icon_bag_alt"></i></a>
                 </li>
                 <li class="quick-view"><RouterLink :to="`/product/${product.id}`">+ Quick View</RouterLink></li>
             </ul>
@@ -33,6 +32,40 @@ export default{
     },
     props: {
         product: Object
+    },
+    data() {
+        return{
+            cart: []
+        }
+    },
+    methods: {
+        saveInCart(id, name, photo, price){
+            const data = {
+                'id': id,
+                'name': name,
+                'photo': photo,
+                'price': price
+            }
+            this.cart.push(data)
+            const parsed = JSON.stringify(this.cart)
+            localStorage.setItem('user_cart', parsed)
+        }
+    },
+    mounted() {
+        
+        if(localStorage.getItem('user_cart')){
+            try {
+                this.cart = JSON.parse(localStorage.getItem('user_cart'))
+            } catch (error) {
+                localStorage.removeItem('user_cart')
+            }
+        }
     }
 }
 </script>
+
+<style>
+.saveInCart{
+    cursor: pointer;
+}
+</style>
